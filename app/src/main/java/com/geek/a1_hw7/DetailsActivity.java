e7abf2c0066d2e2af3bf93a740f37a3f434b72e9
Miraida <miraidajaanbaeva@gmail.com>
2021-03-27 15:01:55 +0600

    initial commit

--- app/src/main/java/com/geek/a1_hw7/DetailsActivity.java
+++ app/src/main/java/com/geek/a1_hw7/DetailsActivity.java
@@ -18,7 +18,6 @@ public class DetailsActivity extends AppCompatActivity {
     private void checkOrientation() {
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
             finish();
-            return;
         }
     }
 
@@ -26,7 +25,8 @@ public class DetailsActivity extends AppCompatActivity {
         UserContactModel model = (UserContactModel) getIntent().getSerializableExtra("EXTRA_MODEL");
         if (model!=null){
             TextFragment textFragment = TextFragment.newInstance(model.getName(),model.getTitle(),model.getImage());
-            getSupportFragmentManager().beginTransaction().add(R.id.details_frame_layout,textFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.details_frame_layout,textFragment).commit();
+            //getSupportFragmentManager().beginTransaction().add(R.id.details_frame_layout,textFragment).commit();
         }
     }
 }
